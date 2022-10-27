b6f08bd22f02d1b9c908d884c12d737b6338f2a4
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2022-10-26 21:39:08 -0300

    #3 feat: add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,4 +9,15 @@ sheetRoutes.get('/expenses-ape', (req: Request, res: Response) => {
   sheetController.expensesApe(req, res);
 });
 
+sheetRoutes.get(
+  '/expenses-monthly/period/:period',
+  (req: Request, res: Response) => {
+    sheetController.expensesMonthly(req, res);
+  }
+);
+
+sheetRoutes.get('/expenses-monthly/pages', (req: Request, res: Response) => {
+  sheetController.pagesSheet(req, res);
+});
+
 export default sheetRoutes;
